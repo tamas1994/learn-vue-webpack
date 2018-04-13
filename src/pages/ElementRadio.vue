390ae853ccea1dceaf8b3378c49d68644d6e461e
tamas1994 <donghaifeng@hotmail.com>
2018-04-13 16:57:29 +0800

    添加radio、Button用例

---
+++ src/pages/ElementRadio.vue
@@ -0,0 +1,110 @@
+<template>
+  <el-container  direction=vertical>
+
+    <h1>基础用法</h1>
+    <el-row>
+      <el-radio v-model="radio" label="1">备选项</el-radio>
+      <el-radio v-model="radio" label="2">备选项</el-radio>
+    </el-row>
+
+    <h1>禁用状态</h1>
+    <el-row>
+      <el-radio disabled v-model="radio" label="禁用">备选项</el-radio>
+      <el-radio disabled v-model="radio" label="选中且禁用">备选项</el-radio>
+    </el-row>
+
+    <h1>单选框组</h1>
+    <el-row>
+      <el-radio-group v-model="radio2">
+        <el-radio :label="3">备选项</el-radio>
+        <el-radio :label="6">备选项</el-radio>
+        <el-radio :label="9">备选项</el-radio>
+      </el-radio-group>
+    </el-row>
+
+    <h1>按钮样式</h1>
+    <div>
+      <el-radio-group v-model="radio3">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio4" size="medium">
+        <el-radio-button label="上海" ></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio5" size="small">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京" disabled ></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio6" disabled size="mini">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+
+
+
+
+
+  <h1>带有边框</h1>
+<!--带边框-->
+    <div>
+      <el-radio v-model="radio7" label="1" border>备选项1</el-radio>
+      <el-radio v-model="radio7" label="2" border>备选项2</el-radio>
+    </div>
+    <div style="margin-top: 20px">
+      <el-radio v-model="radio8" label="1" border size="medium">备选项1</el-radio>
+      <el-radio v-model="radio8" label="2" border size="medium">备选项2</el-radio>
+    </div>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio9" size="small">
+        <el-radio label="1" border>备选项1</el-radio>
+        <el-radio label="2" border disabled>备选项2</el-radio>
+      </el-radio-group>
+    </div>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio10" size="mini" disabled>
+        <el-radio label="1" border>备选项1</el-radio>
+        <el-radio label="2" border>备选项2</el-radio>
+      </el-radio-group>
+    </div>
+
+
+  </el-container>
+
+</template>
+
+
+
+<script>
+  export default {
+    data () {
+      return {
+        radio1: '选中且禁用',
+        radio2: 3,
+        radio3: '上海',
+        radio4: '上海',
+        radio5: '上海',
+        radio6: '上海',
+        radio7: '1',
+        radio8: '1',
+        radio9: '1',
+        radio10: '1'
+      };
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Index from '../pages/Index';
 import ElementContainer from '../pages/ElementContainer.vue';
+import ElementButton from '../pages/ElementButton.vue';
+import ElementRadio from '../pages/ElementRadio.vue';
 
 
 Vue.use(Router);
@@ -17,6 +19,16 @@ export default new Router({
       path: '/element-container',
       name: 'element-container',
       component: ElementContainer
+    },
+    {
+      path: '/element-button',
+      name: 'element-button',
+      component: ElementButton
+    },
+    {
+      path: '/element-radio',
+      name: 'element-radio',
+      component: ElementRadio
     }
   ]
 });
